3b9d0e0e7b3a6b0e2e2cfe23cd2a7db870d8e796
Taylor Watson <taylorwatsonb@gmail.com>
2019-07-12 17:51:43 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+document.body.appendChild(element);
+element.style.textAlign = 'center';
+ul.style.textAlign = 'left';
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
